42f0cb5685e6e9ca2824270fe0ccf9370e6392ea
covenantcodes <covenantcodes@gmail.com>
2025-05-20 20:54:28 +0100

    Added color utility module with predefined color constants

---
+++ utils/colors.ts
@@ -0,0 +1,19 @@
+const colors = {
+    primaryColor: '#4F3F97',
+    secondaryColor: '#E8893F',
+    primaryBg: "#E2FFF6",
+    white: '#ffffff',
+    background: '#FAFAFA',
+    black: '#000000',
+    red: '#DA100B',
+    gray: '#656565',      
+    gray2: '#6B777F',
+    gray3: '#505050',
+    gray4: '##718096',
+    gray5: '#E5E7EB',
+    blue: '#2563EB',
+    deepBlue:'#0C263A',
+    orange: '#E99B36'
+  };
+  
+  export default colors;
